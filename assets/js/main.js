d34e43725d5b49a5c118a281798e4859626bcb38
chibuikemokoli <chibuikemokoli@yahoo.com>
2023-08-07 16:03:50 +0100

    solid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,22 @@
-/*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
   toggle.addEventListener('click', () => {
-    // Add show-menu class to nav menu
     nav.classList.toggle('show-menu')
 
-    // Add show-icon to show and hide the menu icon
     toggle.classList.toggle('show-icon')
   })
 }
 
 showMenu('nav-toggle', 'nav-menu')
 
-/*===== SHOW SCROLL TOP =====*/
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
   if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
-/*===== GSAP ANIMATION =====*/
-// gsap.from('.text-box', {opacity: 0, duration: 5, delay:1.2, x:60})
 gsap.from('.header__image', { opacity: 0, duration: 2, delay: .5, y: -25 })
 gsap.from('.header__content', { opacity: 0, duration: 2, delay: .5, y: 25 })
 gsap.from('.video__image, .hgf, .ab2', { opacity: 0, duration: 2, delay: 1.4, y: 25, ease: 'expo.out' })
@@ -33,7 +27,6 @@ gsap.from('.abts', { opacity: 0, duration: 1.5, delay: .8, y: 25, ease: 'expo.ou
 gsap.from('.nav__list, .ab1', { opacity: 0, duration: 1.2, delay: 1.3, y: 25, ease: 'expo.out' })
 gsap.from('.delp', { opacity: 0, duration: 1.2, delay: 1.3, y: 25, ease: 'expo.out' })
 gsap.from('.delc', { opacity: 0, duration: 1, delay: 1, y: 25, ease: 'expo.out' })
-// gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay:2.3, y:25, ease:'expo.out'})
 gsap.from('.home__img-2', 1.2, {opacity: 0, y: 200, delay: .1})
 gsap.from('.home__img-3', 1.2, {opacity: 0, y: 200, delay: .5})
 gsap.from('.home__data', 1.2, {opacity: 0, y: -60, delay: 1})
@@ -42,7 +35,6 @@ gsap.from('.home__bird-2', 1.2, {opacity: 0, x: 80, delay: 1.2})
 gsap.from('.home__img-1', 1.2, {opacity: 0, y: 200, delay: 1.2})
 gsap.from('.home__img-4', 1.2, {opacity: 0, x: 200, delay: 1.3})
 
-// cookies for website
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -103,7 +95,6 @@ function checkCookie() {
 //   };
 // }
 
-// submit to google sheets
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxmuCz6v88jXsGlhiBkAyAvc8FOxBsPB6dYuWshetc7Bci1FwiiPhZRTsIK9DOkxKzP/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
@@ -121,10 +112,6 @@ form.addEventListener('submit', e => {
         .catch(error => console.error('Error!', error.message))
 })
 
-
-
-
-/*===== CHANGE BACKGROUND HEADER =====*/ 
 function scrollHeader(){
   const header = document.getElementById('header')
   if(this.scrollY >= 200) header.classList.add('scroll-header');
